9abf83391f7b9383d02a8aa1413d8809efa8d067
Thirupathi89 <thirupathi.e@stringmetaverse.com>
2025-08-13 11:51:24 +0530

    files changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-// import "./App.css";
 import ReactGA from 'react-ga';
 import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import TelegramIcon from "@mui/icons-material/Telegram";
